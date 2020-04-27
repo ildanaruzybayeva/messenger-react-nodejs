077fb9abd963fe7c90e236fddb3e79858ef37f7e
Ildana <ildana.ruzybayeva@gmail.com>
2020-04-27 18:32:16 +0000

    Update styles

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -63,7 +63,7 @@ const Chat = ({ location }) => {
                     sendMessage={sendMessage}
                 />
             </div>
-            <TextContainer users={users} />
+            <TextContainer users={users} room={room} />
         </div>
     );
 };
--- client/src/components/TextContainer/TextContainer.js
+++ client/src/components/TextContainer/TextContainer.js
@@ -4,31 +4,11 @@ import onlineIcon from '../../icons/onlineIcon.png';
 
 import './TextContainer.css';
 
-const TextContainer = ({ users }) => (
+const TextContainer = ({ users, room }) => (
     <div className="textContainer">
-        <div>
-            <h1>
-                Realtime Chat Application{' '}
-                <span role="img" aria-label="emoji">
-                    💬
-                </span>
-            </h1>
-            <h2>
-                Created with React, Express, Node and Socket.IO{' '}
-                <span role="img" aria-label="emoji">
-                    ❤️
-                </span>
-            </h2>
-            <h2>
-                Try it out right now!{' '}
-                <span role="img" aria-label="emoji">
-                    ⬅️
-                </span>
-            </h2>
-        </div>
         {users ? (
             <div>
-                <h1>People currently chatting:</h1>
+                <h1>{`People currently in ${room} room:`}</h1>
                 <div className="activeContainer">
                     <h2>
                         {users.map(({ name }) => (
