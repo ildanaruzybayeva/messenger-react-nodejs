6e9c29c0278bbeac9684be3a615b77a0b07fc69d
Ildana <ildana.ruzybayeva@gmail.com>
2020-04-27 16:22:35 +0000

    Setup chatting app

--- server/index.js
+++ server/index.js
@@ -1,25 +1,70 @@
+const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-const http = require('http');
-const PORT = process.env.PORT || 5000;
+const cors = require('cors');
+
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
+
 const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-io.on('connection', socket => {
-    console.log('we have a new connection');
+app.use(cors());
+app.use(router);
 
+io.on('connect', socket => {
     socket.on('join', ({ name, room }, callback) => {
-        console.log(name, room);
+        const { error, user } = addUser({ id: socket.id, name, room });
+
+        if (error) return callback(error);
+
+        socket.join(user.room);
+
+        socket.emit('message', {
+            user: 'admin',
+            text: `${user.name}, welcome to room ${user.room}.`,
+        });
+        socket.broadcast
+            .to(user.room)
+            .emit('message', {
+                user: 'admin',
+                text: `${user.name} has joined!`,
+            });
+
+        io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUsersInRoom(user.room),
+        });
+
+        callback();
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', { user: user.name, text: message });
+
+        callback();
     });
 
     socket.on('disconnect', () => {
-        console.log('user has left');
+        const user = removeUser(socket.id);
+
+        if (user) {
+            io.to(user.room).emit('message', {
+                user: 'Admin',
+                text: `${user.name} has left.`,
+            });
+            io.to(user.room).emit('roomData', {
+                room: user.room,
+                users: getUsersInRoom(user.room),
+            });
+        }
     });
 });
 
-app.use(router);
-
-server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+server.listen(process.env.PORT || 5000, () =>
+    console.log(`Server has started.`),
+);
