16afb3fb27287b813d31ec10c372e75fd65d6b4b
Ildana <ildana.ruzybayeva@gmail.com>
2020-04-27 14:45:39 +0000

    Connect and disconnect users on socket

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,7 +2,30 @@ import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
 
-const Chat = () => {
+let socket;
+
+const Chat = ({ location }) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const ENDPOINT =
+        'https://5000-a718c52f-341a-4136-9028-09197414e7ca.ws-eu01.gitpod.io/';
+
+    useEffect(() => {
+        const { name, room } = queryString.parse(location.search);
+
+        socket = io(ENDPOINT);
+
+        setName(name);
+        setRoom(room);
+
+        socket.emit('join', { name, room });
+
+        return () => {
+            socket.emit('disconnect');
+            socket.off();
+        };
+    }, [ENDPOINT, location.search]);
+
     return <h1>Chat</h1>;
 };
 
--- server/index.js
+++ server/index.js
@@ -1,21 +1,25 @@
 const express = require('express');
-const socketio = require('socket.io')
-const http = require('http')
-const PORT = process.env.PORT || 5000
-const router = require('./router')
+const socketio = require('socket.io');
+const http = require('http');
+const PORT = process.env.PORT || 5000;
+const router = require('./router');
 
-const app = express()
-const server = http.createServer(app)
-const io = socketio(server)
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-app.use(router)
+io.on('connection', socket => {
+    console.log('we have a new connection');
 
-io.on("connection", (socket) => {
-    console.log('we have a new connection')
+    socket.on('join', ({ name, room }, callback) => {
+        console.log(name, room);
+    });
 
-    socket.on("disconnect", ()=> {
-        console.log('user has left')
-    })
+    socket.on('disconnect', () => {
+        console.log('user has left');
+    });
 });
 
-server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
+app.use(router);
+
+server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
